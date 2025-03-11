acd38623a8c32c64cf038e65ef29eda9f06901e4
Nathaniel Patrick L. Ramos <irumaramos123@gmail.com>
2025-03-11 08:42:58 +0800

    hackaton-done

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,8 +3,15 @@
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
 } from 'next-themes'
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
